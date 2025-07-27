32f465fc9021d0a5bad09bf789c9fbda2640727b
Dirk Lemstra <dirk@lemstra.org>
2025-07-27 18:52:28 +0200

    Test non-windows build.

---
+++ .github/build/linux-musl-x64/build.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+set -e
+
+SCRIPT_PATH="$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )"
+SHARED_PATH="$SCRIPT_PATH/../shared"
+
+# Compiler settings
+if [[ "${DEBUG_BUILD}" ]]; then
+  export FLAGS="-g3 -O0 -fPIC -DDEBUG"
+  export CMAKE_BUILD_TYPE="Debug"
+  export MESON_BUILD_TYPE="debug"
+else
+  export FLAGS="-O3 -fPIC -DNDEBUG"
+  export CMAKE_BUILD_TYPE="Release"
+  export MESON_BUILD_TYPE="release"
+fi
+export CMAKE_PREFIX_PATH="/dependencies"
+export CPPFLAGS="-I/dependencies/include"
+export LDFLAGS="-L/dependencies/lib -L/dependencies/lib64"
+export PKG_CONFIG_PATH="/dependencies/lib/pkgconfig:/dependencies/lib64/pkgconfig"
+
+# Shared options
+export CMAKE_COMMAND="cmake"
+export CMAKE_OPTIONS=""
+export CONFIGURE="./configure"
+export CONFIGURE_OPTIONS=""
+export MAKE="make -j$(nproc)"
+export MESON_OPTIONS=""
+
+# Library specific options
+export DE265_OPTIONS=""
+export FONTCONFIG_OPTIONS=""
+export HEIF_OPTIONS=""
+export JPEGTURBO_OPTIONS="-DWITH_SIMD=1"
+export LCMS_OPTIONS=""
+export OPENEXR_OPTIONS=""
+export WEBP_OPTIONS="--enable-libwebpmux --enable-libwebpdemux"
+
+# Build dependencies
+$SHARED_PATH/zlib.sh
+$SHARED_PATH/lzma.sh
+$SHARED_PATH/bzlib.sh
+$SHARED_PATH/zip.sh
+$SHARED_PATH/xml.sh
+$SHARED_PATH/png.sh
+$SHARED_PATH/freetype.sh
+$SHARED_PATH/fontconfig.sh
+$SHARED_PATH/jpeg-turbo.sh
+$SHARED_PATH/tiff.sh
+$SHARED_PATH/webp.sh
+$SHARED_PATH/openjpeg.sh
+$SHARED_PATH/lcms.sh
+$SHARED_PATH/aom.sh
+$SHARED_PATH/de265.sh
+$SHARED_PATH/openh264.sh
+$SHARED_PATH/heif.sh
+$SHARED_PATH/raw.sh
+$SHARED_PATH/deflate.sh
+$SHARED_PATH/exr.sh
+$SHARED_PATH/ffi.sh
+$SHARED_PATH/glib.sh
+$SHARED_PATH/lqr.sh
+$SHARED_PATH/pixman.sh
+$SHARED_PATH/cairo.sh
+$SHARED_PATH/fribidi.sh
+$SHARED_PATH/harfbuzz.sh
+$SHARED_PATH/pango.sh
+$SHARED_PATH/croco.sh
+$SHARED_PATH/rsvg.sh
+$SHARED_PATH/raqm.sh
+$SHARED_PATH/highway.sh
+$SHARED_PATH/brotli.sh
+$SHARED_PATH/jpeg-xl.sh
---
+++ .github/build/linux-musl-x64/install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -e
+
+apk update
+
+apk add autoconf automake bash file g++ gcc gettext-tiny git gperf libtool make musl-libintl nasm pkgconf python3 py3-lxml py3-pip ragel texinfo
+
+pip3 install --upgrade pip
+pip3 install cmake==3.30.3
+pip3 install meson==1.5.1
+pip3 install ninja==1.11.1.1
+
+echo '#!/bin/bash' > /usr/local/bin/gtkdocize
+chmod 755 /usr/local/bin/gtkdocize
