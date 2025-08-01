82c1b2914c077e7ab29126a1fdc0997aeffd6b74
Dirk Lemstra <dirk@lemstra.org>
2025-08-01 11:51:06 +0200

    Updated several dependencies.

--- clone-dependencies.sh
+++ clone-dependencies.sh
@@ -41,15 +41,15 @@ clone_dependencies()
   clone 'croco' '3c35df989a2011f692ee8914b6f054c9f79dc9eb'
   clone 'de265' 'c65aa04018209b75f4d86d5207f67d6396d11973'
   clone 'deflate' '318cc8e7cea3cc7bb6ee76e3da5608ce2883897a'
-  clone 'exr' '24e73fe0d0d7af3254df6125cdfc61530ce920aa'
-  clone 'ffi' '987aa1ff69662a81445c9bbf26fb367e581c36e2'
+  clone 'exr' '58ea96c1b6806b1d48d5173501e2a2fab9b08076'
+  clone 'ffi' '653605c8711ce113c12b3240344a6bd6d82e3a26'
   clone 'freetype' '16c2ff88b9b7dc3b7749074c514a30a2aa990dd7'
   clone 'fribidi' '3c8dc6bb1f668563601c4f470702c2caba02e5a9'
   clone 'glib' '7c18f9c92bd9442ce1924cedd126fe50029416a5'
-  clone 'harfbuzz' '0ed469b3dbd42a372888d8d762443c4cb6f2108c'
+  clone 'harfbuzz' 'a7d34e5028645af933d3e637476bb26f79e53f02'
   clone 'heif' 'cfd076d8da9954ec70f9872adf58bbfc065a6012'
   clone 'highway' '1ca33ea77b33b07ee551666faaf78b6c6bb91832'
-  clone 'jpeg-turbo' '447b5ee728865cdbc16f28ac6e9a385afe84f279'
+  clone 'jpeg-turbo' '0b59952f473a826bf9373702763bc165faad6381'
   clone 'jpeg-turbo-12' '633aacbee5a4fba892d54804a16edeaa9ae68dca'
   clone 'jpeg-turbo-16' '362648691acaf65fbbe3cbcd10db30a66df223cb'
   clone 'jpeg-xl' '0eb0e12ed9cfe95e35e88f8e144517d5f6e4e093'
@@ -59,15 +59,15 @@ clone_dependencies()
   clone 'openh264' 'e88db93e997c4c69f224aefc33d35c4b26578f80'
   clone 'openjpeg' 'ae7744d2680e126abf93eaa6191996db446c04fb'
   clone 'pango' '9b019fb87547855243b148dee4e782658f56225e'
-  clone 'pixman' '89c85ea1f3df2ec2ccba6c357beae4efa8c9fbfb'
-  clone 'png' 'dff27c4fe95b3f9e9f1c42f30516908e736cad8c'
+  clone 'pixman' '1c8b41cc9f38f5a018ada19b32da641f5782be11'
+  clone 'png' '4742e03a92b910ad869da14dd96b9c096e853326'
   clone 'raqm' '2070ec3f2a4ed5d7c96cd1c9a5e46484fc064eec'
   clone 'raw' 'a91ae551812a270b3a4331de8dc1468a3485272c'
   clone 'rsvg' '873ccacb328ff93b22dc13f3ad554110200247ed'
   clone 'tiff' 'd1d17ce3f9f5ba2215edabd3d54ea0f46bb7538c'
-  clone 'webp' 'c84afca5ced87cbffaea3dfa9babecf6dd9b7f1d'
-  clone 'xml' '387cc8b3dae70ff20cf4c98dc3c261256f644c9e'
-  clone 'zip' '4b0c16f239d742b00739aee2e4b7afa6a73f3c0a'
+  clone 'webp' '444cf23c2e177105bc876611ec3d82b7d23502a0'
+  clone 'xml' '132d758d137345cbebd80417f26a216ac01ac168'
+  clone 'zip' '987138bb74e6f18b464246b1f025de82c52167f9'
   clone 'zlib' '42998011c390732967bf56316c6dd0cb3872e589'
 
   cd ..
@@ -96,7 +96,7 @@ clone_linux_dependencies()
 
   cd "LinuxDependencies"
 
-  clone 'fontconfig' 'e22253fa48865b43027261409e71df64ed3c9d16'
+  clone 'fontconfig' '9f848be61d3dfcafcc76e083499123153c525e0b'
 
   cd ..
 }
