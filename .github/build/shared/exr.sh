9a9ba922c72537343c8443023f44a61f535b5440
Dirk Lemstra <dirk@lemstra.org>
2025-08-01 18:45:50 +0200

    Updated non-windows builds to use the new imath repository.

--- .github/build/shared/exr.sh
+++ .github/build/shared/exr.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 set -e
 
-cd Dependencies/exr/imath
-
-rm ImathConfig.h
-
-$CMAKE_COMMAND . $CMAKE_OPTIONS -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX -DBUILD_SHARED_LIBS=off -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE -DBUILD_TESTING=off -DCMAKE_CXX_FLAGS="$FLAGS"
-$MAKE install
-
-cd ../openexr
+cd Dependencies/exr
 
 rm IexConfig.h
 rm IexConfigInternal.h
