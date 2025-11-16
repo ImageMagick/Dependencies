655e58512f8e62de5543b4374ed6aff2a34c9a41
Dirk Lemstra <dirk@lemstra.org>
2025-11-16 17:12:14 +0100

    Changed pip install options

--- .github/build/shared/fontconfig.sh
+++ .github/build/shared/fontconfig.sh
@@ -5,7 +5,7 @@ cd NonWindowsDependencies/fontconfig
 
 autoreconf -fiv
 chmod +x ./configure
-pip3 install lxml
-pip3 install six
+pip3 install --break-system-packages lxml
+pip3 install --break-system-packages six
 $CONFIGURE $CONFIGURE_OPTIONS --enable-libxml2 --enable-static=yes --disable-nls --disable-shared $FONTCONFIG_OPTIONS  --prefix=$INSTALL_PREFIX CFLAGS="$FLAGS"
 $MAKE install
