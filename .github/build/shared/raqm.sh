3c9303a7e5ba8a9368c29d369211b5a87111cc0f
Dirk Lemstra <dirk@lemstra.org>
2026-02-12 20:51:44 +0100

    Updated dependencies.

--- .github/build/shared/raqm.sh
+++ .github/build/shared/raqm.sh
@@ -5,5 +5,5 @@ cd Dependencies/raqm
 
 mkdir __build
 cd __build
-CFLAGS=$FLAGS meson setup .. $MESON_OPTIONS --buildtype=$MESON_BUILD_TYPE --prefix=$INSTALL_PREFIX --default-library=static -Ddocs=false
+CFLAGS=$FLAGS meson setup .. $MESON_OPTIONS --buildtype=$MESON_BUILD_TYPE --prefix=$INSTALL_PREFIX --default-library=static -Dtests=false
 ninja install
