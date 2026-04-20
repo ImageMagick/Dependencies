f2d8e86f147b00a2fa40facfbe7f8f78230578a2
Dirk Lemstra <dirk@lemstra.org>
2026-04-20 20:35:19 +0200

    Run pip3 install with --break-system-packages instead.

--- .github/build/linux-musl-x64/install.sh
+++ .github/build/linux-musl-x64/install.sh
@@ -5,10 +5,10 @@ apk update
 
 apk add autoconf automake bash file g++ gcc gettext-tiny git gperf libtool linux-headers make musl-libintl nasm pkgconf python3 py3-lxml py3-pip ragel texinfo zip
 
-pip3 install --no-cache-dir --upgrade pip
-pip3 install --no-cache-dir cmake==3.30.3
-pip3 install --no-cache-dir meson==1.5.1
-pip3 install --no-cache-dir ninja==1.11.1.1
+pip3 install --break-system-packages --upgrade pip
+pip3 install --break-system-packages cmake==3.30.3
+pip3 install --break-system-packages meson==1.5.1
+pip3 install --break-system-packages ninja==1.11.1.1
 
 echo '#!/bin/bash' > /usr/local/bin/gtkdocize
 chmod 755 /usr/local/bin/gtkdocize
