948cc0390398401f1ecd7f559be10094fb0437fc
Dirk Lemstra <dirk@lemstra.org>
2026-07-03 12:07:19 +0200

    Fix for building cairo.

--- .github/build/shared/cairo.sh
+++ .github/build/shared/cairo.sh
@@ -8,7 +8,7 @@ rm src/config.h
 
 mkdir __build
 cd __build
-CFLAGS=$FLAGS meson setup .. $MESON_OPTIONS --buildtype=$MESON_BUILD_TYPE --prefix=$INSTALL_PREFIX --default-library=static -Dxlib=disabled -Dquartz=disabled -Dfontconfig=enabled -Dfreetype=enabled -Dtests=disabled
+CFLAGS="$FLAGS -DHAVE_CTIME_R" meson setup .. $MESON_OPTIONS --buildtype=$MESON_BUILD_TYPE --prefix=$INSTALL_PREFIX --default-library=static -Dxlib=disabled -Dquartz=disabled -Dfontconfig=enabled -Dfreetype=enabled -Dtests=disabled
 
 $SED -i 's/#define CAIRO_HAS_PTHREAD 1/#define CAIRO_NO_MUTEX 1/' config.h
 
