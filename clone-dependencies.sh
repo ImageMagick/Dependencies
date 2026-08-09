3800d7d441b71a6bbf5581f2ba231040a13afe80
Dirk Lemstra <dirk@lemstra.org>
2026-08-09 09:35:59 +0200

    Only clone the dependencies that are used.

--- clone-dependencies.sh
+++ clone-dependencies.sh
@@ -37,47 +37,64 @@ clone_dependencies()
 
   clone 'aom' 'f74a4670353c01c194379a7ef922858c530acbd2'
   clone 'brotli' '6225e7a93b6bcc1f3e3b267e9523a8eaea49eb5e'
-  clone 'bzip2' 'abffe764f875f71d051efb19d4c83139375f82d7'
-  clone 'cairo' '95c037dd6c455d314d12a2fad76b07b0a34c171b'
-  clone 'croco' '178785cdc6faecad555a4e6fe4fda2b625046e8e'
   clone 'de265' '80e57d4a56dd9bd725e1d937be8060fbffc36ca1'
   clone 'exr' '93cdfc71e4778d1e5aeabc567dd60f9181b7029f'
   clone 'ffi' '6cb36a5f0bb37226679e508bc192d89a609674fa'
   clone 'freetype' '6cf8b1a08bdec1d41246f51bcd12e5a62c9845e2'
-  clone 'fribidi' 'ef903b6f8c90cba6141afd0f28e1ca6c130317f3'
-  clone 'gdk-pixbuf' '57b955e47bfa3634a310368816cb829d7812e430'
   clone 'glib' '016373fad6b5253d531fc1bb34af23883db75056'
-  clone 'harfbuzz' 'b498653966b2cd5a3e574d3b4828cf66b4d0ecf7'
   clone 'heif' '5def4181577ada890da9b8cb0b913f93f24c715e'
   clone 'highway' 'e1a586af5be6358bb983ff608f9678a622209ca3'
   clone 'imath' 'da111679be3f9a0fb4b79d5adebfca1546873983'
   clone 'jpeg-turbo' 'b91492930ceb23c0b5282e8b9fc21de54182d92e'
-  clone 'jpeg-turbo-12' '9837071e8c0e5fba6daa03f31aba4416ff5eaf7c'
-  clone 'jpeg-turbo-16' 'd16d0839d655478b4d517ebe2aeabfecce8192d9'
   clone 'jpeg-xl' '8503118ff13ab8b11680296900da3361d6703dc1'
   clone 'lcms' 'd261ab6fda30fd7578a2ce7ace5c0b2c7d3fc0b8'
   clone 'lqr' 'a2e9e8b5b41bf0569f51482707a12d354a086f3a'
-  clone 'lzma' '3eb7dab2a3d6e8f5ebe5f02a657355afe2c6459d'
-  clone 'openh264' '5ed6416df5c4eb35e275f759792c9253c5f1f5f3'
   clone 'openjpeg' '5a52e8e0690da686ddb6e98310e7bb5190a9a7c5'
   clone 'openjph' 'a8d14ac14a3ea23067ef140dced2e53691a697fa'
-  clone 'pango' '0b1e4536e0005f3c36b523c17ea12322490efd45'
-  clone 'pixman' 'f7b55a8d3c39380724da9fbdef72ad70be800efc'
   clone 'png' '88ee938ef91b0289339be463b29003f65d486210'
-  clone 'raqm' 'a699b8b7838acb4ac8d5891f42ecf3d84a00a9ad'
   clone 'raw' '9f7613ef48877180225b9377c87b0f3a65a2ca8e'
-  clone 'rsvg' '7415a6765db7554ba6f0f1b3dcb0877f36ff581b'
   clone 'tiff' 'a00846bca0212faca9306ae7bdd1c89e3e46bf82'
   clone 'webp' 'b981ef267195cb12f2cb97e4dd23e12a1323a4ce'
   clone 'xml' '4509bf0d7b7a501b682da8635527cd77525b0ec9'
-  clone 'zip' '745377bdab21cf5e5fc19d0bbfcde93030d3af5e'
   clone 'zlib' 'e42706934e4e7564d960031ae62ff29d918ee06d'
+
+  if [ "$platform" = "wasm" ]; then
+    cd ..
+    return
+  fi
+
+  clone 'bzip2' 'abffe764f875f71d051efb19d4c83139375f82d7'
+  clone 'cairo' '95c037dd6c455d314d12a2fad76b07b0a34c171b'
+  clone 'croco' '178785cdc6faecad555a4e6fe4fda2b625046e8e'
+  clone 'fribidi' 'ef903b6f8c90cba6141afd0f28e1ca6c130317f3'
+  clone 'gdk-pixbuf' '57b955e47bfa3634a310368816cb829d7812e430'
+  clone 'harfbuzz' 'b498653966b2cd5a3e574d3b4828cf66b4d0ecf7'
+  clone 'lzma' '3eb7dab2a3d6e8f5ebe5f02a657355afe2c6459d'
+  clone 'openh264' '5ed6416df5c4eb35e275f759792c9253c5f1f5f3'
+  clone 'pango' '0b1e4536e0005f3c36b523c17ea12322490efd45'
+  clone 'pixman' 'f7b55a8d3c39380724da9fbdef72ad70be800efc'
+  clone 'raqm' 'a699b8b7838acb4ac8d5891f42ecf3d84a00a9ad'
+  clone 'rsvg' '7415a6765db7554ba6f0f1b3dcb0877f36ff581b'
+  clone 'zip' '745377bdab21cf5e5fc19d0bbfcde93030d3af5e'
+
+  case "$platform" in
+    "" | windows) ;;
+    *) cd ..; return ;;
+  esac
+
+  clone 'jpeg-turbo-12' '9837071e8c0e5fba6daa03f31aba4416ff5eaf7c'
+  clone 'jpeg-turbo-16' 'd16d0839d655478b4d517ebe2aeabfecce8192d9'
 
   cd ..
 }
 
 clone_optional_dependencies()
 {
+  case "$platform" in
+    "" | windows) ;;
+    *) return ;;
+  esac
+
   if [ ! -d "OptionalDependencies" ]; then
     mkdir -p "OptionalDependencies"
   fi
@@ -93,6 +110,10 @@ clone_optional_dependencies()
 
 clone_non_windows_dependencies()
 {
+  if [ "$platform" = "wasm" ]; then
+    return
+  fi
+
   if [ ! -d "NonWindowsDependencies" ]; then
     mkdir -p "NonWindowsDependencies"
   fi
@@ -103,6 +124,8 @@ clone_non_windows_dependencies()
 
   cd ..
 }
+
+platform=${1:-}
 
 clone_dependencies
 clone_optional_dependencies
